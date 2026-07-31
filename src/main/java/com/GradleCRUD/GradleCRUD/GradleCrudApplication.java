8ed4c3685eff2c01e19fffed43caa3043a68b8c9
Madhushan-Dev <chandrasegaranmadhushan02@gmail.com>
2026-07-31 08:51:16 +0530

    My Gradle CRUD

---
+++ src/main/java/com/GradleCRUD/GradleCRUD/GradleCrudApplication.java
@@ -0,0 +1,13 @@
+package com.GradleCRUD.GradleCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GradleCRUD/GradleCRUD/GradleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GradleCRUD.GradleCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
