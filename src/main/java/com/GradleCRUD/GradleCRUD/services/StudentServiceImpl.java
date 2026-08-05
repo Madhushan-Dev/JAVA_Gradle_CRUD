4897839d50636c5fab1443c5714a777802b03e94
Madhushan-Dev <chandrasegaranmadhushan02@gmail.com>
2026-08-05 21:53:29 +0530

    Service updated

--- src/main/java/com/GradleCRUD/GradleCRUD/services/StudentServiceImpl.java
+++ src/main/java/com/GradleCRUD/GradleCRUD/services/StudentServiceImpl.java
@@ -1,11 +1,15 @@
 package com.GradleCRUD.GradleCRUD.services;
 
+import com.GradleCRUD.GradleCRUD.dto.requestDTO.RequestDTO;
+import com.GradleCRUD.GradleCRUD.dto.responseDTO.ResponseDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService{
     @Override
-    public wor
+    public ResponseDTO createStudent(RequestDTO requestDTO){
+
+    }
 
     return null;
 }
