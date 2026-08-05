1fab55ad1b5d384f51ca9ba4c53612ee15a84e27
Madhushan-Dev <chandrasegaranmadhushan02@gmail.com>
2026-08-05 22:03:20 +0530

    Updated Service

--- src/main/java/com/GradleCRUD/GradleCRUD/services/StudentServiceImpl.java
+++ src/main/java/com/GradleCRUD/GradleCRUD/services/StudentServiceImpl.java
@@ -2,14 +2,27 @@ package com.GradleCRUD.GradleCRUD.services;
 
 import com.GradleCRUD.GradleCRUD.dto.requestDTO.RequestDTO;
 import com.GradleCRUD.GradleCRUD.dto.responseDTO.ResponseDTO;
+import com.GradleCRUD.GradleCRUD.entities.Student;
+import com.GradleCRUD.GradleCRUD.mapper.StudentMapper;
+import com.GradleCRUD.GradleCRUD.repositories.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService{
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @Autowired
+    private StudentMapper studentMapper;
+
     @Override
     public ResponseDTO createStudent(RequestDTO requestDTO){
+        //DTO->Entity
+        Student student= StudentMapper.toEntity(requestDTO);
 
+        Student savedStudent =studentRepo.save(student);
+
+        return studentMapper.toDto(savedStudent);
     }
-
-    return null;
 }
